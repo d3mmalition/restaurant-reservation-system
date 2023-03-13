d03784e22f27415995567cc639e5d3f31d15fb37
d3mmalition <ecarr.gardner@gmail.com>
2023-03-13 18:06:04 -0400

    adding db urls

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://ddhxefvj:KXk7LumD8Qh6vhXcTanDDfBUtTlna_sz@salt.db.elephantsql.com/ddhxefvj",
+  DATABASE_URL_DEVELOPMENT = "postgres://dekpndza:UTi0FPa_26TyY_8fBzl0RFMiJ4zm5enx@salt.db.elephantsql.com/dekpndza",
+  DATABASE_URL_TEST = "postgres://hlesvhwu:JfSuGAq2kl_zEd-VZc5491I1yBjaAMZX@salt.db.elephantsql.com/hlesvhwu",
+  DATABASE_URL_PREVIEW = "postgres://luaivyky:6-B2ShliamQiyytIxvygMeakSLKSfwY4@salt.db.elephantsql.com/luaivyky",
   DEBUG,
 } = process.env;
 
