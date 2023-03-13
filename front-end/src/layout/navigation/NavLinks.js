61d5ecd816134eaa997579fa348eed2d8a1a5e64
d3mmalition <ecarr.gardner@gmail.com>
2023-03-13 14:27:48 -0400

    updated moment dep

--- front-end/src/layout/navigation/NavLinks.js
+++ front-end/src/layout/navigation/NavLinks.js
@@ -1,8 +1,4 @@
 import { Link } from "react-router-dom";
-import { RiDashboard3Line } from "react-icons/ri";
-import { FiSearch } from "react-icons/fi";
-import { AiOutlinePlusCircle } from "react-icons/ai";
-import { HiOutlineCircleStack } from "react-icons/hi2";
 import classes from "./NavBar.module.css";
 import React from "react";
 
@@ -15,7 +11,6 @@ const NavLinks = () => {
           style={{ display: "flex", justifyContent: "center" }}
           to="/dashboard"
         >
-          <RiDashboard3Line className={classes.menu_icons} />
           <div className={classes.menu_text}>&nbsp;Dashboard</div>
         </Link>
       </li>
@@ -25,7 +20,6 @@ const NavLinks = () => {
           style={{ display: "flex", justifyContent: "center" }}
           to="/search"
         >
-          <FiSearch className={classes.menu_icons} />
           <div className={classes.menu_text}>&nbsp;Search</div>
         </Link>
       </li>
@@ -35,7 +29,6 @@ const NavLinks = () => {
           style={{ display: "flex", justifyContent: "center" }}
           to="/reservations/new"
         >
-          <AiOutlinePlusCircle className={classes.menu_icons} />
           <div className={classes.menu_text}>&nbsp;New Reservation</div>
         </Link>
       </li>
@@ -45,7 +38,6 @@ const NavLinks = () => {
           style={{ display: "flex", justifyContent: "center" }}
           to="/tables/new"
         >
-          <HiOutlineCircleStack className={classes.menu_icons} />
           <div className={classes.menu_text}>&nbsp;New Table</div>
         </Link>
       </li>
